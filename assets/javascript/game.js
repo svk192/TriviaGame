165abecc848ff6370fa6a802c7bf39016d424eea
svk192 <sabaviqar@gmail.com>
2019-06-21 00:53:31 -0400

    bug fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             options: ["cars", "food", "lipstick"],
             answer: "lipstick",
             image: "https://media.giphy.com/media/l4Ki6CgyyOU6IIO6A/giphy.gif"
-  
+
         },
 
         {
@@ -13,7 +13,7 @@ $(document).ready(function () {
             options: ["Sandwiches", "Salad", "Pizza"],
             answer: "Sandwiches",
             image: "https://media.giphy.com/media/pyEDsTDy2aIR5SXkBD/giphy.gif"
-         
+
         },
 
         {
@@ -21,42 +21,43 @@ $(document).ready(function () {
             options: ["Masseuse", "Chef", "Agent"],
             answer: "Chef",
             image: "https://media.giphy.com/media/IdyHPMy8eqZ68/giphy.gif"
-  
+
         },
         {
             question: "Whose catchphrase is 'Oh My God!'?",
             options: ["Janice", "Monica", "Ross"],
             answer: "Janice",
             image: "https://media.giphy.com/media/fYPtYT18vVLm8/giphy.gif"
-  
+
         },
         {
             question: "What is the name of the Coffee Shop?",
             options: ["Friends Cafe", "Central Perk", "Central Park"],
             answer: "Central Perk",
             image: "https://media.giphy.com/media/6KoHLujBN4QWk/giphy.gif"
-  
+
         },
         {
             question: "Who was the bride in the first one?",
             options: ["Pheobe", "Monica", "Rachel"],
             answer: "Rachel",
             image: "https://media.giphy.com/media/w5M9QgelugIJG/giphy.gif"
-  
+
         },
-    
+
     ];
 
-   var questionNumber = [0,1,2,3,4,5]; 
-   shuffle(questionNumber);
+    var questionNumber = [0, 1, 2, 3, 4, 5];
+    shuffle(questionNumber);
     var clockRunning = false;
     var intervalId;
     var wins = 0;
     var losses = 0;
     var counter = 0;
     var questionNumber = 0;
-    var delayButtonAlert;
-
+    // var timeOut;
+    var timeOutShort;
+    var timeOutLong;
     var time = 30;
 
 
@@ -73,7 +74,7 @@ $(document).ready(function () {
     $(document).on("click", ".option", function () {
         console.log("counter" + counter);
         // event.preventDefault();//
-        
+
         checkAnswer($(this).attr("data-letter"));
 
     });
@@ -85,6 +86,9 @@ $(document).ready(function () {
         if (counter < 5) {
 
             reset();
+
+            clearTimeout(timeOutShort);
+            clearTimeout(timeOutLong);
             displayQuestion();
 
         }
@@ -92,27 +96,27 @@ $(document).ready(function () {
             var message;
             var image = $("<img>");
 
-            if(wins >=losses){
+            if (wins >= losses) {
 
-            image.attr("src", "https://media.giphy.com/media/j9T65td3Kjqko/giphy.gif");
+                image.attr("src", "https://media.giphy.com/media/j9T65td3Kjqko/giphy.gif");
                 message = "You are the winner!"
-        }
-        else{
-            image.attr("src", "https://media.giphy.com/media/10I5e2yNnaozOo/giphy.gif");
+            }
+            else {
+                image.attr("src", "https://media.giphy.com/media/10I5e2yNnaozOo/giphy.gif");
                 message = "Better Luck Next Time!"
-        }
-        
-        $("#container").html("<p>" + "End!" + "</p><br> Wins:" 
-            + wins + "<br> Losses:"
-             + losses + "<br>" 
-            + message+ "<br>" 
-             );
-             $("#container").append(image);
+            }
+
+            $("#container").html("<p>" + "End!" + "</p><br> Wins:"
+                + wins + "<br> Losses:"
+                + losses + "<br>"
+                + message + "<br>"
+            );
+            $("#container").append(image);
 
             stop();
-            
-        
-        
+
+
+
         }
 
 
@@ -130,26 +134,26 @@ $(document).ready(function () {
 
             $("#display").empty();
 
-           
-            $("#result").html("<p>" + "Time Out!" + "</p>" +
-            "<p>" + "Correct Answer: " + questions[questionNumber].answer+ "</p>");
 
-            delayButtonAlert = setTimeout(nextquestion, 2000);
+            $("#result").html("<p>" + "Time Out!" + "</p>" +
+                "<p>" + "Correct Answer: " + questions[questionNumber].answer + "</p>");
+
+            timeOutShort = setTimeout(nextquestion, 2000);
 
         }
 
 
-       else if (data === questions[questionNumber].answer) {
+        else if (data === questions[questionNumber].answer) {
             console.log("Correct");
             wins++;
             $("#Options").empty();
             $("#Question").empty();
 
             $("#display").empty();
-            $("#result").html("<p>" + "Correct!" + "</p>"+
-            "<p>" + "Correct Answer: " + questions[questionNumber].answer+ "</p>");
+            $("#result").html("<p>" + "Correct!" + "</p>" +
+                "<p>" + "Correct Answer: " + questions[questionNumber].answer + "</p>");
 
-            delayButtonAlert = setTimeout(nextquestion, 2000);
+            timeOutShort = setTimeout(nextquestion, 2000);
 
         }
         else {
@@ -159,27 +163,27 @@ $(document).ready(function () {
             $("#Question").empty();
             $("#display").empty();
 
-            delayButtonAlert = setTimeout(nextquestion, 2000);
+            timeOutShort = setTimeout(nextquestion, 2000);
 
-            $("#result").html("<p>" + "Not Correct!" + "</p>"+
-            "<p>" + "Correct Answer: " + questions[questionNumber].answer+ "</p>");
-            
+            $("#result").html("<p>" + "Not Correct!" + "</p>" +
+                "<p>" + "Correct Answer: " + questions[questionNumber].answer + "</p>");
+
 
 
         }
         var image = $("<img>");
-            image.attr("src",questions[questionNumber].image);
-            $("#result").append(image);
+        image.attr("src", questions[questionNumber].image);
+        $("#result").append(image);
     }
 
 
     function displayQuestion() {
 
 
-            questionNumber = counter;   
+        questionNumber = counter;
 
         $("#Question").html(questions[questionNumber].question);
-        shuffle( questions[questionNumber].options);
+        shuffle(questions[questionNumber].options);
         for (i = 0; i < 3; i++) {
 
             var newDiv = $("<div>");
@@ -192,12 +196,12 @@ $(document).ready(function () {
         }
 
         counter++;
-        delayButtonAlert = setTimeout(checkAnswer, 30000,0);
+        timeOutLong = setTimeout(checkAnswer, 30000, 0);
     }
 
 
 
-    function shuffle ( array ) { array . sort ( ( ) => Math . random ( ) - 0.5 ) ; } 
+    function shuffle(array) { array.sort(() => Math.random() - 0.5); }
 
 
 
@@ -208,6 +212,7 @@ $(document).ready(function () {
         if (!clockRunning) {
             intervalId = setInterval(count, 1000);
             clockRunning = true;
+
         }
     }
 
@@ -226,7 +231,6 @@ $(document).ready(function () {
 
     function reset() {
         stop();
-
         $("#Options").empty();
         $("#Question").empty();
         $("#result").empty();
